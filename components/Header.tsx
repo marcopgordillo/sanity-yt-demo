4f0216adea322d567a90d20ff783a909847b70ed
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2022-07-06 14:47:37 -0400

    Finish front page

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+function Header() {
+  return (
+    <header className="flex justify-between p-5">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="Logo" />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <Link href="#">About</Link>
+          <Link href="#">Contact</Link>
+          <Link href="#">
+            <a className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</a>
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <Link href="#">Sign In</Link>
+        <Link href="#">
+          <a className="border border-gray-600 px-4 py-1 rounded-full">Get Started</a>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,83 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          This is Medium 2.0
-        </h1>
+      <Header />
 
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">Medium</span> is a place to write, read and connect
+          </h1>
+          <p>It's easy and free to post your thinking or any topic and connect with millions of readers.</p>
+        </div>
+        
+        <img className="hidden md:inline-flex h-32 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="Logo" />
+      </div>
 
+      {/* Posts */}
+
+      <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg shadow-md overflow-hidden group cursor-pointer">
+              <img 
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!} 
+                alt=""
+              />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <h3 className="text-lg font-bold">{post.title}</h3>
+                  <p className="text-xs">{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+              </div>
+            </div>
+          </Link>
+        ))}
       </main>
 
     </div>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `
+      *[_type=="post"]{
+      _id,
+      title,
+      slug,
+      author -> {
+      name,
+      image,
+    },
+    description,
+    mainImage,
+    }
+  `
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
